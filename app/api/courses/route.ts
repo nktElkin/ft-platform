371b2235d835f6486b27b50fb92250b86405610a
nktElkin <nkte.main@gmail.com>
2024-12-26 16:16:49 +0100

    course creation + prisma schema update

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -1,36 +1,52 @@
+import { auth } from "@/auth";
 import { db } from "@/lib/db";
-import { connect } from "http2";
 import { NextResponse } from "next/server";
 
-// export async function POST (req: Request, res: Response) {
-//     try{
-//         const isTutor = true; 
-//         // if (!accountId) {
-//         //     return new NextResponse("Unauthorized", { status: 401 });
-//         // }
-//         if (!isTutor) return new NextResponse("no right for creation", { status: 401 });
-//        const {title} = await req.json(); 
-//        const course = await db.course.create({
-//               data: {
-//                 title,
-//                 author:{
-//                     connect: {
-//                         id: "676bd9998d0990b31b0e70ca"
-//                     }
-//                 },
-//                 category:{
-//                     connect:{
-//                         id: "676bdcfb8d0990b31b0e70cf"
-//                     }
-//                 }
-//               }
-//        });
-//        return NextResponse.json({course});
-//     } catch (error) {
-//         console.error(error);
-//         return new NextResponse("Failed to create course", { status: 500 });
-//     }
-// }
+export async function POST (req: Request, res: Response) {
+    try{
+        const session = await auth();
+        if (!session) return new NextResponse("Unauthorized", { status: 401 });
+
+
+        // find user via filter
+        const user = await db.user.findUnique({
+            where: {
+                email: session?.user?.email || "",
+            }
+        });
+        console.log("user: ", user);
+        const category = await db.category.findUnique({
+            where: {
+                categoryName: "Uncategorized"
+            }
+        });
+        console.log("category: ", category);
+        if (!user) return new NextResponse("User not found", { status: 401 });
+        if (user?.role === "STUDENT") return new NextResponse("Permission denied", { status: 401 });
+
+
+       const {title} = await req.json(); 
+       const course = await db.course.create({
+              data: {
+                title,
+                author:{
+                    connect: {
+                        id: user?.id
+                    }
+                },
+                category:{
+                    connect: {
+                        id: category?.id
+                    }
+                },
+              }
+       });
+       return NextResponse.json({course});
+    } catch (error) {
+        console.error('[CREATE-COURSE]',error);
+        return new NextResponse("Failed to create course", { status: 500 });
+    }
+}
 
 // export async function GET (req: Request, res: Response) {
 //     try{
